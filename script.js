1178ef4865c684deb32234bf93782e60e8029549
pares-seux <paresseux.992@gmail.com>
2022-02-01 16:55:47 +0400

    Fixed#3

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const title = prompt("Как называется ваш проект?");
-const screens = prompt("Какие типы экранов нужно разработать?");
+const screens = prompt("Какие типы экранов нужно разработать?" + "");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
 const rollback = 50;
 const adaptive = confirm("Нужен ли адаптив на сайте?");
 const services = createArray();
 const fullPrice = screenPrice + services[0].price + services[1].price;
-const servicePercentPrice = Math.round(fullPrice - rollback);
+const rollbackPrice = fullPrice * (rollback / 100);
+const servicePercentPrice = Math.round(fullPrice - rollbackPrice);
 
 function createArray() {
   const array = [];
@@ -19,7 +20,24 @@ function createArray() {
   }
   return array;
 }
-
+console.log(
+  "Типы данных: \n title - " +
+    typeof title +
+    ",\n fullPrice - " +
+    typeof fullPrice +
+    ",\n adaptive - " +
+    typeof adaptive +
+    "."
+);
+console.log("Длина строки: " + screens.length + ".");
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(screens.toLowerCase().split(", "));
+console.log("Процент отката посреднику за работу " + rollbackPrice);
 console.log("Итоговая стоимость: " + servicePercentPrice);
 switch (true) {
   case fullPrice >= 30000:
