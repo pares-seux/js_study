73550481d4875a33785cb664d23777c6863a6d33
pares-seux <paresseux.992@gmail.com>
2022-02-01 15:51:09 +0400

    Fixed

--- script.js
+++ script.js
@@ -4,32 +4,33 @@ const title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать?");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
 const rollback = 50;
-let adaptive = false;
-const adaptiveAnswer = prompt("Нужен ли адаптив на сайте?");
-if (
-  adaptiveAnswer === "да" ||
-  adaptiveAnswer === "true" ||
-  adaptiveAnswer === "нужен"
-) {
-  adaptive = true;
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const services = createArray();
+const fullPrice = screenPrice + services[0].price + services[1].price;
+const servicePercentPrice = Math.round(fullPrice - rollback);
+
+function createArray() {
+  const array = [];
+  for (let i = 0; i < 2; i++) {
+    let service = {};
+    service["name"] = prompt("Какой дополнительный тип услуги нужен?");
+    service["price"] = +prompt("Сколько это будет стоить?");
+    array.push(service);
+  }
+  return array;
 }
-let services = [];
-for (let i = 0; i < 2; i++) {
-  let service = {};
-  service["name"] = prompt("Какой дополнительный тип услуги нужен?");
-  service["price"] = +prompt("Сколько это будет стоить?");
-  services.push(service);
-}
-let fullPrice = screenPrice + services[0].price + services[1].price;
-let servicePercentPrice = Math.round(fullPrice - rollback);
 
 console.log("Итоговая стоимость: " + servicePercentPrice);
-if (fullPrice >= 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice < 30000 && fullPrice >= 15000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000 && fullPrice >= 0) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что то пошло не так");
+switch (true) {
+  case fullPrice >= 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice < 30000 && fullPrice >= 15000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice < 15000 && fullPrice >= 0:
+    console.log("Скидка не предусмотрена");
+    break;
+  default:
+    console.log("Что то пошло не так");
 }
